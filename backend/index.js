6a96b9e954dc87da0118d8e72faecba51dfa8c4c
Sudhanshu Patil <w7t996w6x4@privaterelay.appleid.com>
2026-07-07 13:50:36 +0530

    fix: force IPv4 resolution for nodemailer on Render

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const nodemailer = require('nodemailer');
+require('dns').setDefaultResultOrder('ipv4first');
 require('dotenv').config();
 
 const app = express();
